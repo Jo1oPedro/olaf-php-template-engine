6a4395eae99a68f5bb701569b3b544f7ef96b46a
João Pedro <joao.pedreira@estudante.ufjf.br>
2023-03-24 19:22:14 -0300

    Only php files are allowed

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 //include the loader
 require_once 'SimpleTemplateEngine/loader.php';
 
-$env = new SimpleTemplateEngine\Environment('examples', '.php');
+use SimpleTemplateEngine\Environment;
+
+$env = new Environment('examples', '.php');
 echo $env->render('home', ['date'=>date('l jS \of F Y')]);
